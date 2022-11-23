76b98572e0c846959b58ae9b772c3500a6a5d215
hand_cheng.xing <cheng.xing@hand-china.com>
2022-11-23 17:10:48 +0800

    order服务上传

---
+++ src/main/java/com/example/demoorderservice/DemoOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOrderServiceApplication.class, args);
+    }
+
+}
